414d4932b87ce113d68e8447e60d963600021fad
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2023-11-15 11:18:28 +0530

    Update SpringJenkinsPipelineGroovyApplication.groovy
    to verify build trigger

--- src/main/groovy/com/myapp/spring/SpringJenkinsPipelineGroovyApplication.groovy
+++ src/main/groovy/com/myapp/spring/SpringJenkinsPipelineGroovyApplication.groovy
@@ -2,7 +2,7 @@ package com.myapp.spring
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-
+// some changes done
 @SpringBootApplication
 class SpringJenkinsPipelineGroovyApplication {
 
